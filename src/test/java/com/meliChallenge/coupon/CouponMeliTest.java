3e128c04e776f10e3777efed080d92a6e67d9273
Oscar Aldana <oealdana@gmail.com>
2022-10-13 06:27:36 -0500

    Merge remote-tracking branch 'origin/main'

--- src/test/java/com/meliChallenge/coupon/CouponMeliTest.java
+++ src/test/java/com/meliChallenge/coupon/CouponMeliTest.java
@@ -5,19 +5,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
 
 import com.meliChallenge.coupon.dto.ItemDTO;
+import com.meliChallenge.coupon.service.CouponMeliService;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
 
-public class MachineTests {
+public class CouponMeliTest {
 
+    @Autowired
+    ApplicationContext context;
     @Test
-    public void whenAllItemsCostLessReturnAllItems(){
+    void contextLoads() {
+        CouponMeliService couponMeliService = context.getBean(CouponMeliService.class);
+        assert (couponMeliService instanceof CouponMeliService);
 
-        List <ItemDTO> items = Machine.getItems();
-        Integer totalItems = (int) items.stream().count();
-        List <ItemDTO> calculatedItems = Machine.calculate(Machine.getItems(), Machine.getCouponValue());
-        Integer totalCalculatedItems = (int) calculatedItems.stream().count();
-        assertEquals(true, (totalItems == totalCalculatedItems));
+        /*CouponController couponController = ac.getBean(CouponController.class);
+        assert (couponController instanceof CouponController);
+
+        Coupon coupon = new Coupon();
+        assert (coupon instanceof Coupon);
+        coupon.setItemIds(new ArrayList<String>());
+
+        CouponRequest couponRequest = new CouponRequest((float) 0);
+        assert (couponRequest instanceof CouponRequest);
+
+        CouponRequest couponRequest1= new CouponRequest(new ArrayList<String>(), (float) 0);
+        assert (couponRequest1 instanceof CouponRequest);
+        couponRequest.setAmount((float) 10);
+
+        CouponResponse couponResponse = new CouponResponse(new ArrayList<String>(), (float) 0);
+        assert (couponResponse instanceof CouponResponse);
+
+        CouponResponse couponResponse1 = new CouponResponse((float) 0);
+        assert (couponResponse1 instanceof CouponResponse);
+        couponResponse.setTotal((float) 10);*/
     }
 
 
